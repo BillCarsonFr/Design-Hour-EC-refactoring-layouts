46a9edce18095288bd82556ba24a07e81458795a
Valere <bill.carson@valrsoft.com>
2026-03-23 19:25:47 +0100

    make participantViewModel using the model

--- src/grug/components/tiles/ParticipantTileViewModel.ts
+++ src/grug/components/tiles/ParticipantTileViewModel.ts
@@ -1,4 +1,7 @@
 import { BaseViewModel } from "../../viewmodel/BaseViewModel.ts";
+import type { Session } from "../../session/session.ts";
+import { combineLatest, filter, map, switchMap } from "rxjs";
+import type { Participant } from "../../session/participant.ts";
 
 export interface ParticipantTileSnapshot {
   tileId: string;
@@ -10,14 +13,51 @@ export interface ParticipantTileSnapshot {
 }
 
 export interface ParticipantTileViewProps {
-  vm: ParticipantTileViewModel;
+  participantId: string;
+  session: Session;
 }
 
 export class ParticipantTileViewModel extends BaseViewModel<
   ParticipantTileSnapshot,
   ParticipantTileViewProps
 > {
-  constructor(props: ParticipantTileViewProps) {
-    super(props, props.vm.snapshot.current);
+  constructor({ participantId, session }: ParticipantTileViewProps) {
+    super(
+      { participantId, session },
+      {
+        tileId: participantId,
+        displayName: participantId,
+        isSpeaking: false,
+        isMuted: false,
+        isVideoEnabled: false,
+      },
+    );
+
+    const sub = session.participants$
+      .pipe(
+        map((participants) => participants.find((p) => p.id === participantId)),
+        filter((p): p is Participant => p !== undefined),
+        switchMap((participant) =>
+          combineLatest([
+            participant.displayName,
+            participant.isSpeaking$,
+            participant.isMuted$,
+            participant.isVideoEnabled$,
+          ]).pipe(
+            map(([displayName, isSpeaking, isMuted, isVideoEnabled]) => ({
+              tileId: participantId,
+              displayName: displayName ?? participantId,
+              isSpeaking,
+              isMuted,
+              isVideoEnabled,
+            })),
+          ),
+        ),
+      )
+      .subscribe((snapshot) => {
+        this.snapshot.set(snapshot);
+      });
+
+    this.disposables.track(() => sub.unsubscribe());
   }
 }
