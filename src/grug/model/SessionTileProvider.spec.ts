94b53a11b8cb18650a27419f8cb98af50714ea16
Valere <bill.carson@valrsoft.com>
2026-03-23 11:10:25 +0100

    First tile provider tests

---
+++ src/grug/model/SessionTileProvider.spec.ts
@@ -0,0 +1,105 @@
+import {it, expect, describe, beforeEach} from "vitest";
+
+import {SessionTileProvider} from "./SessionTileProvider";
+import {Participant} from "../session/participant.ts";
+import {BehaviorSubject, tap} from "rxjs";
+import type {Session} from "../session/session.ts";
+import type {TileMetaData} from "../layout/LayoutEngine.ts";
+import {TestScheduler} from "rxjs/testing";
+
+
+describe("SessionTileProvider", () => {
+
+    let scheduler: TestScheduler;
+    let mockSession: Session;
+    let provider: SessionTileProvider;
+
+    beforeEach(() => {
+        scheduler = new TestScheduler(() => {
+            // We do assertions manually below
+        });
+
+        const participants$ = new BehaviorSubject<Participant[]>([]);
+        mockSession = {
+            participants$: participants$,
+        } as unknown as Session;
+        provider = new SessionTileProvider(mockSession);
+    })
+
+    it("Initial ordering", async () => {
+
+        scheduler.run(({flush}) => {
+            const alice = new Participant("0", "alice");
+            const bob = new Participant("1", "bob");
+            const carl = new Participant("2", "carl");
+
+            // Initial state
+            bob.isSpeaking$.next(true);
+            carl.isVideoEnabled$.next(true);
+
+            const emissions: TileMetaData[][] = [];
+            provider.tiles$
+                .pipe(tap(console.log))
+                .subscribe((tiles) => emissions.push(tiles));
+
+            scheduler.schedule(() => {
+                mockSession.participants$.next([alice, bob, carl]);
+            });
+
+            flush();
+
+            const currentState = emissions.pop();
+            expect(currentState).toBeDefined();
+            expect(currentState?.length).toBe(3);
+            expect(currentState?.[0]?.stableId).toBe("1");
+            expect(currentState?.[1]?.stableId).toBe("2");
+            expect(currentState?.[2]?.stableId).toBe("0");
+
+        });
+    })
+
+
+    it("The active speaker should be on the top", async () => {
+
+        scheduler.run(({flush}) => {
+            const alice = new Participant("0", "alice");
+            const bob = new Participant("1", "bob");
+            const carl = new Participant("2", "carl");
+
+
+            const emissions: TileMetaData[][] = [];
+            provider.tiles$
+                .pipe(tap(console.log))
+                .subscribe((tiles) => emissions.push(tiles));
+
+            scheduler.schedule(() => {
+                mockSession.participants$.next([alice, bob, carl]);
+            });
+
+            // carl starts speaking
+            carl.isSpeaking$.next(true);
+
+            flush();
+
+            expect(emissions.pop()?.[0]?.stableId).toBe(carl.id);
+
+            // bob starts speaking
+            bob.isSpeaking$.next(true);
+            carl.isSpeaking$.next(false);
+
+            flush();
+            expect(emissions.pop()?.[0]?.stableId).toBe(bob.id);
+
+            // Alice starts speaking
+            alice.isSpeaking$.next(true);
+            bob.isSpeaking$.next(false);
+
+            flush();
+            expect(emissions.pop()?.[0]?.stableId).toBe(alice.id);
+
+
+        });
+    })
+
+
+});
